c62d7ef6438a72e2f8540f0c8917a4c520a79ce7
Jake Towers <JakeTowers@users.noreply.github.com>
2023-10-28 19:30:32 +0100

    Favourites Route

---
+++ components/Header.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+enum Routes {
+  HOME = "/",
+  FAVOURITES = "/favourites",
+}
+
+const Header = () => {
+  const pathname = usePathname();
+
+  return (
+    <div className="flex space-x-10 border-b-2 p-4 ">
+      <Link
+        className={`${pathname === Routes.HOME && "font-bold"}`}
+        href={Routes.HOME}
+      >
+        Repositories
+      </Link>
+      <Link
+        className={`${pathname === Routes.FAVOURITES && "font-bold"}`}
+        href={Routes.FAVOURITES}
+      >
+        Favourites
+      </Link>
+    </div>
+  );
+};
+
+export default Header;
